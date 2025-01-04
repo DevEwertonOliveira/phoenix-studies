fb2869f87206a5cff8e9c8233e4a59b30dbb8661
ewerton2007 <ewerton2007@gmail.com>
2025-01-04 16:03:55 +0000

    estudo elixir com phoenix via idx

---
+++ lib/projeto_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule ProjetoWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use ProjetoWeb, :html
+
+  embed_templates "page_html/*"
+end
